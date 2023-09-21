18fb387083d5c2fa2bdf98b8ac7ebf9c5a68cb79
999tommy <adegboyetommy@gmail.com>
2023-09-21 17:57:50 +0100

    'new commit'

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getGoal, setGoal, UpdateGoal, deleteGoal } = require('../controllers/goalcontroller')
+const { getGoal, setGoal, UpdateGoal, deleteGoal } = require('../controllers/goalController')
 
 router.route('/').get(getGoal).post(setGoal)
-router.route('/:id').delete(deleteGoal).post(UpdateGoal)
-
-router.get('/', getGoal)
-
-router.post('/', setGoal)
-
-router.put('/:id', UpdateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/:id').delete(deleteGoal).put(UpdateGoal)
 
 
 module.exports = router 
